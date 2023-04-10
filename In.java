fea6fca8dc94e85fd3e14149aba1e437177a3d79
Ifhaam Zuhree <mzmifhaam@gmail.com>
2023-04-10 07:45:31 +0530

    Update In.java

--- In.java
+++ In.java
@@ -2,58 +2,71 @@
 import java.util.*;
 public class In
 {
-    // initilaise the scanner object "in" with the System.in stream
-    // this allows the Scanner to read input from the terminal
+    // Initilaise the scanner object "in" with the System.in stream 
     private static Scanner in = new Scanner(System.in);
     
-    // Read the next line of text entered by the user 
+    /*
+     * Method name: nextLine
+     * Parameters: none
+     * Description: - Read the next line of text entered by the user
+                    - Return the input as a String
+     */
     public static String nextLine()
     {
-        // return the input as a String
         return in.nextLine();
     }
 
-    // Read the next line of text entered by the user
+    /*
+     * Method name: nextChar
+     * Parameters: none
+     * Description: - Read the next line of text entered by the user
+                    - Return the first character of the input as a Char
+     */
     public static char nextChar()
     {
-        // return the first character of the input as a Char
         return in.nextLine().charAt(0);
     }
     
-    // Read the next line of text entered by the user
+    /*
+     * Method name: nextUpperChar
+     * Parameters: none
+     * Description: - Read the next line of text entered by the user
+                    - Return the char at index zero as an uppercase Char
+     */
     public static char nextUpperChar()
     {
-        // returns the char at index zero as an uppercase Char
         return in.nextLine().toUpperCase().charAt(0);
     }
     
-    // Reads the integer entered by the user 
+    /*
+     * Method name: nextInt
+     * Parameters: none
+     * Description: - Reads the integer entered by the user
+                    - Assigns the input by the user to local variable i
+                    - Returns the integer entered by the user
+     *  Note: - When the user enters an integer and presses "Enter", a newline character (\n) is added to the input stream, which is not consumed by nextInt().
+              - Consuming this newline character is important because it ensures that the next input operation starts on a new line, avoiding any potential errors or unexpected behavior.
+     */       
     public static int nextInt()
     {
-        // assigns the input by the user to local variable i
         int i = in.nextInt();
-        // consumes the new line character that is left behind the input stream
-        /*
-         * When the user enters an integer and presses "Enter", a newline character (\n) is added to the input stream, which is not consumed by nextInt(). 
-         * Consuming this newline character is important because it ensures that the next input operation starts on a new line, avoiding any potential errors or unexpected behavior.
-         */
         in.nextLine();
-        // returns the integer entered by the user
         return i;
     }
     
-    // Reads the double entered by the user  
+    /*
+     * Method name: nextDouble
+     * Parameters: none
+     * Description: - Reads the double entered by the user
+                    - Assigns the input by the user to local variable d
+                    - Returns the double entered by the user
+     *  Note: - When the user enters an double and presses "Enter", a newline character (\n) is added to the input stream, which is not consumed by nextDouble().
+              - Consuming this newline character is important because it ensures that the next input operation starts on a new line, avoiding any potential errors or unexpected behavior.
+     */     
     public static double nextDouble()
     {
-        // assigns the input by the user to local variable d
         double d = in.nextDouble();
-        // consumes the new line character that is left behind the input stream
-        /*
-         * When the user enters a double and presses "Enter", a newline character (\n) is added to the input stream, which is not consumed by nextDouble(). 
-         * Consuming this newline character is important because it ensures that the next input operation starts on a new line, avoiding any potential errors or unexpected behavior.
-         */
         in.nextLine();
-        // returns the double entered by the user
         return d;   
     }
 }
