fca67e60c0f6dfd6c2632f938b3701fa324a22be
Ifhaam Zuhree <mzmifhaam@gmail.com>
2023-04-11 13:35:06 +0530

    Drink and Drinks class
    Completed the two classes - added the counter as well

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,11 @@
+import classes.Drink;
+import classes.Drinks;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) 
+    {
+        Drinks allDrinks = new Drinks();
+        allDrinks.displayColdDrinks();
+        allDrinks.displayHotDrinks();
     }
 }
---
+++ src/main/java/classes/Drink.java
@@ -0,0 +1,83 @@
+package classes;
+
+public class Drink extends Item 
+{
+    // attributes
+    protected String pairing;
+    
+    /*
+     * Constructor Name : Drink()
+     * Parameters : name, price, rating, pairing
+     * Description : Alternate constructor
+     */
+    public Drink(String name, double price, int rating, Category category,String pairing) 
+    {
+        super(name, price, rating, category);
+        this.pairing = pairing;
+    }
+
+    /*
+     * Method Name : getPairing()
+     * Parameters : none
+     * Description : getter/accessor
+     */
+    public String getPairing() 
+    {
+        return pairing;
+    }
+
+    /*
+     * Method Name : getName()
+     * Parameters : none
+     * Description : getter/accessor
+     */
+    @Override
+    public String getName() 
+    {
+        return name;
+    }
+
+    /*
+     * Method Name : getPrice()
+     * Parameters : none
+     * Description : getter/accessor
+     */    
+    @Override
+    public double getPrice() 
+    {
+        return price;
+    }
+
+    /*
+     * Method Name : getRating()
+     * Parameters : none
+     * Description : getter/accessor
+     */
+    @Override
+    public int getRating() 
+    {
+        return rating;
+    }
+
+    /*
+     * Method Name : getCategory()
+     * Parameters : none
+     * Description : getter/accessor
+     */
+    @Override
+    public Category getCategory() 
+    {
+        return category;
+    }
+
+    /*
+     * Method Name : toString()
+     * Parameters : none
+     * Description : display attributes of the Item objects
+     */
+    @Override
+    public String toString() 
+    {
+        return "Name: " + name + ", Price: " + price + ", Rating: " + rating + ", Category: " + category + ", Pairing: " + pairing;
+    }
+}
