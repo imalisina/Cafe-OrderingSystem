1978292987e7ad3edc2c4ce8cad872add48fb16f
Ifhaam Zuhree <mzmifhaam@gmail.com>
2023-04-11 14:08:41 +0530

    Update Snacks.java

--- src/main/java/classes/Snacks.java
+++ src/main/java/classes/Snacks.java
@@ -4,23 +4,40 @@ import java.util.LinkedList;
 
 public class Snacks 
 {
+    // attributes
+    // LinkedList data for all the Snack items
     private LinkedList<Snack> allSnacks = new LinkedList<Snack>();
 
+    /*
+     * Constructor Name : Snacks()
+     * Parameters : none
+     * Description : Default constructor
+     */
     public Snacks() 
     {
-        setUpAllSnacks();
+        snackSeeder();
     }
     
-    private void setUpAllSnacks()
+    /*
+     * Method Name : drinkSeeder()
+     * Parameters : none
+     * Description : - create all snack objects
+     */
+    private void snackSeeder()
     {
-        allSnacks.add(new Snack("Falafel", 6.99, Category.SNACK,4, "Small", 4));
-        allSnacks.add(new Snack("Bandari Sausage", 4.99, Category.SNACK,3, "Medium", 2));
+        allSnacks.add(new Snack("Falafel", 6.99, Category.SNACK, 4, "Small", 4));
+        allSnacks.add(new Snack("Bandari Sausage", 4.99, Category.SNACK, 3, "Medium", 2));
         allSnacks.add(new Snack("Çig Köfte", 3.49, Category.SNACK, 4, "Large", 1));
         allSnacks.add(new Snack("Kalamata Olive Hummus", 4.99, Category.SNACK, 5, "Medium", 1));
         allSnacks.add(new Snack("Biltong", 2.99, Category.SNACK, 4, "Small", 3));
         allSnacks.add(new Snack("Brik", 1.99, Category.SNACK, 4, "Small", 3));
     }
 
+    /*
+     * Method Name : displayAllSnacks()
+     * Parameters : none
+     * Description : display all items that are in the SNACK drinks category
+     */
     public void displayAllSnacks()
     {
         System.out.println("Snacks Menu:\n");
