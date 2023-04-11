aceb113e19af758ce9d5c94df170a930d243cb48
Alisina Barghandan <barghandanalisina@gmail.com>
2023-04-11 21:55:33 +0530

    Update Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,10 +1,7 @@
 import java.security.NoSuchAlgorithmException;
 
-import classes.Users.Users;
-
 public class Main {
     public static void main(String[] args) throws NoSuchAlgorithmException { 
-        Users all = new Users();
-        all.registerOperation();
+        System.out.println("Hello World");
     }
 }
