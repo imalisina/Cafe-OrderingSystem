1a239835cbde227f34c358711e1de6898cb7e100
Alisina Barghandan <barghandanalisina@gmail.com>
2023-04-13 20:19:52 +0530

    Relations are updated

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,11 +5,5 @@ import classes.Users.Users;
 public class Main {
     public static void main(String[] args) throws NoSuchAlgorithmException { 
         System.out.println("Hello World");
-
-        Users full = new Users();
-        full.registerOperation();
-        full.loginOperation();
-        full.editProfileHandler();
-        full.allUsers.get(1);
     }
 }
