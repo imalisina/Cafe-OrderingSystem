bf174882f1058230b64c79d681ddb1796f1ef5ef
Alisina Barghandan <barghandanalisina@gmail.com>
2023-04-28 00:20:30 +0530

    Adding Router and Main classes

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,71 @@
+import classes.System.Input;
+import classes.System.Menu;
+import classes.System.Router;
+
 public class Main {
+    // Declare & initialize program name
+    private static final String PROGRAM_NAME = "Persian Nights - Cafe Ordering System";
+    private static final String DEVELOPER_NAME_ONE = "Alisina Br.";
+    private static final String DEVELOPER_NAME_TWO = "Ifhaam Z";
+
+    // New instance of Router class
+    static Router newRouter;
+
     public static void main(String[] args) { 
-        System.out.println("Hello World");
+        // Operations
+        welcomeMessage();
+        displayAuthMenu();
+
+        // Send entered router option
+        callRouter();
+
+        // Using Router's alternate constructor
+        System.out.println("[INFO] Option " + newRouter.getRouter() + " selected.");
+        newRouter = new Router(true);
+
+        switch(newRouter.getRouter()) {
+            case 1:
+                // ADD REGISTRATION OPERATION
+                break;
+            case 2:
+                // ADD LOGIN OPERATION
+                break;
+        }
+    }
+
+    /*
+     * Method Name : welcomeMessage()
+     * Parameters : none
+     * Description : display a welcome message in terminal window
+    */
+    public static void welcomeMessage() {
+        System.out.println("----------------------");
+        System.out.println("Welcome to " + PROGRAM_NAME);
+        System.out.println("Code by " + DEVELOPER_NAME_ONE + " & " + DEVELOPER_NAME_TWO);
+    }
+
+    /*
+     * Method Name : displayAuthMenu()
+     * Parameters : none
+     * Description : display the program's first menu
+    */
+    public static void displayAuthMenu() {
+        Menu authMenu = new Menu("Authentication");
+        String[] authMenuItems = {"Getting Started","Login"};
+        authMenu.showMenu(authMenuItems);
+    }
+
+    /*
+    * Method name: sendRouter
+    * Parameters: none
+    * Description: gets router input from users
+    */
+    public static void callRouter() {
+        // Local router variable
+        int router;
+        // New instance of Router by default constructor
+        newRouter = new Router();
+        router = Input.nextInt();
+        newRouter.setRouter(router);
     }
 }
---
+++ src/main/java/classes/System/Router.java
@@ -0,0 +1,76 @@
+package classes.System;
+
+public class Router {
+    // An attribute for routing in switch statement
+    private static int router;
+
+    // An attribute for activating alternate constructor
+    private static boolean activateRouter = false;
+
+    /*
+    * Method name: Router --> DEFAULT CONSTRUCTOR
+    * Parameters: none
+    * Description: ask user to select one option
+    */
+    public Router() {
+        System.out.print("Please select one option : ");
+    }
+
+    /*
+    * Method name: Router --> ALTERNATE CONSTRUCTOR
+    * Parameters: boolean activateRouter
+    * Description: validates the router option and
+    *              execute other operations
+    */
+    public Router(boolean activateRouter){
+        if (activateRouter) {
+            // Validate option
+            routerValidator();
+        } else {
+            System.exit(0);
+        }
+    }
+
+    /*
+     * Method name: routerValidator
+     * Parameters: none
+     * Description: validating entered menu option
+     */
+    public static void routerValidator () {
+        if ((router != 1) && (router != 2)) {
+            System.out.println("Invalid option! Please try again.");
+            System.exit(0);
+        } else {
+            activateRouter = true;
+            System.out.println("Please wait for a second ....");
+            System.out.println("----------------------");
+        }
+    }
+
+    /*
+    * Method name: getRouter
+    * Parameters: none
+    * Description: a Getter method(Accessor) for the router
+    */
+    public int getRouter () {
+        return router;
+    }
+
+    /*
+    * Method name: setRouter
+    * Parameters: int router
+    * Description: a Setter method(Mutator) for the router
+    */
+    public void setRouter (int router) {
+        Router.router = router;
+    }
+
+    /*
+    * Method name: getGateway
+    * Parameters: none
+    * Description: a Getter method(Accessor) for the activateRouter attribute
+    */
+    public boolean getActivateRouter () {
+        return activateRouter;
+    }
+}
